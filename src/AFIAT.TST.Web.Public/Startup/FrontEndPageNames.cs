34d71b269a7c3e836ece5bd5c3141dd8883c69ff
ghader <gharibyan.ghader@gmail.com>
2021-10-29 20:37:46 +0330

    styles fix and page names

--- src/AFIAT.TST.Web.Public/Startup/FrontEndPageNames.cs
+++ src/AFIAT.TST.Web.Public/Startup/FrontEndPageNames.cs
@@ -2,7 +2,10 @@
 {
     public class FrontEndPageNames
     {
-        public const string Home = "Frontend.Home";
-        public const string About = "Frontend.About";
+        public const string Home ="صفحه اول";
+        public const string Register = "ثبت نام";
+        public const string Login = "ورود به حساب";
+
+        public const string About = "درباره ما";
     }
 }
