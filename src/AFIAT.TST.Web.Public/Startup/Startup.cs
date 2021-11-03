802ad891301823f47f7b5f2046ecbee0794b3131
ghader <gharibyan.ghader@gmail.com>
2021-11-03 23:33:10 +0330

    remove runtime compile

--- src/AFIAT.TST.Web.Public/Startup/Startup.cs
+++ src/AFIAT.TST.Web.Public/Startup/Startup.cs
@@ -41,7 +41,7 @@ namespace AFIAT.TST.Web.Public.Startup
             services.AddControllersWithViews(options =>
             {
                 options.Filters.Add(new AutoValidateAntiforgeryTokenAttribute());
-            }).AddNewtonsoftJson().AddRazorRuntimeCompilation();
+            }).AddNewtonsoftJson();
 
             if (bool.Parse(_appConfiguration["KestrelServer:IsEnabled"]))
             {
