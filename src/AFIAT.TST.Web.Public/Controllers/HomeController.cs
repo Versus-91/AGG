c79970d343fba13659eb6585a6810129e415c0d8
ghader <gharibyan.ghader@gmail.com>
2021-12-12 21:44:55 +0330

    main page changes

--- src/AFIAT.TST.Web.Public/Controllers/HomeController.cs
+++ src/AFIAT.TST.Web.Public/Controllers/HomeController.cs
@@ -1,13 +1,21 @@
 using Microsoft.AspNetCore.Mvc;
 using AFIAT.TST.Web.Controllers;
+using AFIAT.TST.Pages;
+using System.Threading.Tasks;
 
 namespace AFIAT.TST.Web.Public.Controllers
 {
     public class HomeController : TSTControllerBase
     {
-        public ActionResult Index()
+        public IItemsAppService _itemsService;
+        public HomeController(IItemsAppService itemsService)
         {
-            return View();
+            _itemsService = itemsService;
+        }
+        public async Task<ActionResult> Index()
+        {
+            var pages = await _itemsService.GetAll(new Pages.Dtos.GetAllItemsInput { MaxResultCount = 15});
+            return View(pages);
         }
         public ActionResult About()
         {
